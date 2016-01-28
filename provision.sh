cced5b52572bf21d03ec1590dafa3089f6871e0f
François Drouin-Morin <francois.drouinm@gmail.com>
2016-01-28 07:36:05 -0500

    Provisioning breakthroughs I managed to find a way to install MySQL through a shell script. The rest should follow quickly.

--- provision.sh
+++ provision.sh
@@ -1,17 +1,78 @@
-#Initial setup
-apt-get update
-apt-get -y install git-all debconf-utils
+#!/usr/bin/env bash
+export DEBIAN_FRONTEND=noninteractive
 
-#Apache setup
-apt-get -y install apache2
+sudo aptitude update -q
 
-#MYSQL setup
-#
-#Setup for script handling of password prompt
-#Default password for dev workstation password: TerraArcana
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password TerraArcana'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password TerraArcana'
-apt-get -y install mysql-server
+PASSWORD='12345678'
+# Force a blank root password for mysql
+echo "mysql-server mysql-server/root_password password $PASSWORD" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password $PASSWORD" | debconf-set-selections
 
-#PHP setup
-apt-get -y install php5-gd libssh2-php php5-cli php5-mysql
+# Install mysql, nginx, php5-fpm
+sudo aptitude install -q -y -f mysql-server mysql-client nginx php5-fpm
+
+# Install commonly used php packages
+sudo aptitude install -q -y -f php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcached php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-xcache
+
+sudo rm /etc/nginx/sites-available/default
+sudo touch /etc/nginx/sites-available/default
+
+sudo cat >> /etc/nginx/sites-available/default <<'EOF'
+server {
+  listen   80;
+
+  root /usr/share/nginx/html;
+  index index.php index.html index.htm;
+
+  # Make site accessible from http://localhost/
+  server_name _;
+
+  location / {
+    # First attempt to serve request as file, then
+    # as directory, then fall back to index.html
+    try_files $uri $uri/ /index.html;
+  }
+
+  location /doc/ {
+    alias /usr/share/doc/;
+    autoindex on;
+    allow 127.0.0.1;
+    deny all;
+  }
+
+  # redirect server error pages to the static page /50x.html
+  #
+  error_page 500 502 503 504 /50x.html;
+  location = /50x.html {
+    root /usr/share/nginx/html;
+  }
+
+  # pass the PHP scripts to FastCGI server listening on /tmp/php5-fpm.sock
+  #
+  location ~ \.php$ {
+    try_files $uri =404;
+    fastcgi_split_path_info ^(.+\.php)(/.+)$;
+    fastcgi_pass unix:/var/run/php5-fpm.sock;
+    fastcgi_index index.php;
+    include fastcgi_params;
+  }
+
+  # deny access to .htaccess files, if Apache's document root
+  # concurs with nginx's one
+  #
+  location ~ /\.ht {
+    deny all;
+  }
+
+
+}
+EOF
+
+sudo touch /usr/share/nginx/html/info.php
+sudo cat >> /usr/share/nginx/html/info.php <<'EOF'
+<?php phpinfo(); ?>
+EOF
+
+sudo service nginx restart
+
+sudo service php5-fpm restart
