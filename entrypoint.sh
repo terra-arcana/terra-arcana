70b99191bb34a6b08d416f24fe0b3a4eb736b90c
François Drouin-Morin <francois.drouinm@gmail.com>
2016-03-11 14:10:36 -0500

    Added checks for WP-CLI operations
    Added conditions to verify the existance of files, folders and database tables to not execute WP-CLI on an already configured instance of the application.

--- entrypoint.sh
+++ entrypoint.sh
@@ -57,26 +57,26 @@ if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 	fi
 fi
 
-echo $WORDPRESS_DB_NAME
-echo $WORDPRESS_DB_USER
-echo $WORDPRESS_DB_PASSWORD
-echo $WORDPRESS_DB_HOST
-
 sleep 20
+if [ ! -e wp-config.php ]; then
+	echo "Creating wp-config.php"
+	sudo -u www-data wp core config --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST
+fi
 
-echo "Creating wp-config.php"
-sudo -u www-data wp core config --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST
+if ! $(wp core is-installed); then
+	echo "Creating database"
+	sudo -u www-data wp db create
 
-echo "Creating database"
-sudo -u www-data wp db create
+	echo "Creating tables"
+	sudo -u www-data wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+fi
 
-echo "Creating tables"
-sudo -u www-data wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+if [ ! -d /var/html/www/wp-content/themes/terra-arcana]
+	echo "Copying theme to /var/www/html/wp-content/themes/terra-arcana folder"
+	sudo -u www-data cp -r /usr/src/wordpress/wp-content/themes/terra-arcana /var/www/html/wp-content/themes/terra-arcana
 
-echo "Copying theme to /var/www/html/wp-content/themes/wp-content folder"
-sudo -u www-data cp -r /usr/src/wordpress/wp-content/themes/terra-arcana /var/www/html/wp-content/themes/terra-arcana
-
-echo "Installing Terra theme"
-sudo -u www-data wp theme activate terra-arcana
+	echo "Installing Terra theme"
+	sudo -u www-data wp theme activate terra-arcana
+fi
 
 exec "$@"
