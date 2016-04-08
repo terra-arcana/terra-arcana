c488ddda81d86417b4cfdbf88da06a55122f5ac9
François Drouin-Morin <francois.drouinm@gmail.com>
2016-04-07 21:46:06 -0400

    Tweaked plugin installation

--- entrypoint.sh
+++ entrypoint.sh
@@ -80,12 +80,12 @@ if [ ! -d /var/html/www/wp-content/themes/terra-arcana]; then
 fi
 
 # TODO find how to check if installed
-sudo -u www-data wp plugin install rest-api
-sudo -u www-data wp plugin install OAuth1-master
-sudo -u www-data wp plugin install $ACFP_URL
+sudo -u www-data wp plugin install rest-api --force --activate
+#sudo -u www-data wp plugin install OAuth1-master
+sudo -u www-data wp plugin install $ACFP_URL --force --activate
 
-sudo -u www-data wp plugin activate rest-api
-sudo -u www-data wp plugin activate OAuth1-master
-sudo -u www-data wp plugin activate 'Advanced Custom Fields Pro'
+#sudo -u www-data wp plugin activate rest-api
+#sudo -u www-data wp plugin activate OAuth1-master
+#sudo -u www-data wp plugin activate 'Advanced Custom Fields Pro'
 
 exec "$@"
