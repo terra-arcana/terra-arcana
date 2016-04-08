8418e0c9a7caf1578d2b37f3907b6161b4699359
François Drouin-Morin <francois.drouinm@gmail.com>
2016-04-07 22:38:57 -0400

    Tweaked script again

--- entrypoint.sh
+++ entrypoint.sh
@@ -68,7 +68,7 @@ if ! $(wp core is-installed); then
 	sudo -u www-data wp db create
 
 	echo "Creating tables"
-	sudo -u www-data wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+	sudo -u www-data wp core install --url=$WP_URL --title="$WP_TITLE" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 fi
 
 if [ ! -d "/var/html/www/wp-content/themes/terra-arcana" ]; then
