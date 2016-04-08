702d871f1cd550fb1f787234da39e8987b354cc7
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-04-07 21:22:40 -0400

    Fixed errors on missing WP_API_Settings on unit tests

--- webpack.tests.js
+++ webpack.tests.js
@@ -1,4 +1,8 @@
-var context = require.context('./', true, 
+var context = require.context('./', true,
 	/(app\/tests|src\/views\/tests).*\/[^\/]+\.test\.jsx$/);
+
+window.WP_API_Settings = {
+	root: 'localhost:80/wp-json'
+};
 
 context.keys().forEach(context);
