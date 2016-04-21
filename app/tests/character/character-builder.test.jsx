1a07efc136a6f8f6f70865e64ace4685569a689c
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-04-21 00:32:13 -0400

    Updated NPM packages

--- app/tests/character/character-builder.test.jsx
+++ app/tests/character/character-builder.test.jsx
@@ -1,6 +1,7 @@
 /* global describe, it */
 
-var TestUtils = require('react-addons-test-utils'),
+var React = require('react'), // eslint-disable-line no-unused-vars
+	TestUtils = require('react-addons-test-utils'),
 	expect = require('expect'),
 	CharacterBuilder = require('../../scripts/character/character-builder.jsx');
 
