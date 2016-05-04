46bf51f2e3dc93c68c5ad41dee0048fe49696d91
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-05-03 21:39:45 -0400

    v0.2.1 (#132)
    * Added favicon and HTML title
    
    fix #115
    
    * Changed ingredient list to 2016 version
    
    * Prepare 2.1 release (#131)

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 <head>
 	<?php wp_head(); ?>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo get_bloginfo('name') . ' | ' . get_bloginfo('description'); ?></title>
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 </head>
 <body>
 	<?php require_once('app/index.html'); ?>
