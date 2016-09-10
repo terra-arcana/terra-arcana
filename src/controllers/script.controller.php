98bb76fa122d9c197062d5f1c2e89ee351f56183
Frédéric Bolduc <bolducfrederic@gmail.com>
2016-09-10 10:28:37 -0400

    [WIP] Conversion to Bootstrap 4

--- src/controllers/script.controller.php
+++ src/controllers/script.controller.php
@@ -28,12 +28,14 @@ namespace terraarcana {
 		public function enqueue_scripts() {
 			$base = get_stylesheet_directory_uri() . '/';
 
+			wp_register_script('tether', 'https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js');
+
 			if (isset($_GET['debug'])) {
-				wp_register_script('bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.js', array('jquery'));
+				wp_register_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.js', array('jquery', 'tether'));
 
 				wp_enqueue_script('app', $base . 'dist/app.js', array('bootstrap'), null, true);
 			} else {
-				wp_register_script('bootstrap-min', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js', array('jquery'));
+				wp_register_script('bootstrap-min', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js', array('jquery', 'tether'));
 
 				// FIXME: Enqueue minified scripts in production
 				wp_enqueue_script('app', $base . 'dist/app.js', array('bootstrap-min'), null, true);
@@ -56,8 +58,8 @@ namespace terraarcana {
 			$base = get_stylesheet_directory_uri() . '/';
 
 			wp_enqueue_style('gfonts', 'https://fonts.googleapis.com/css?family=Cinzel+Decorative:400,700|PT+Serif:400,700,700italic,400italic|PT+Serif+Caption');
-			wp_enqueue_style('bootstrap', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap.min.css');
-			wp_enqueue_style('bootstrap-theme', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/css/bootstrap-theme.min.css', array('bootstrap'));
+			wp_enqueue_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css');
+			//wp_enqueue_style('bootstrap-theme', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap-theme.min.css', array('bootstrap'));
 			wp_enqueue_style('terra-arcana', $base . 'dist/terra-arcana.css');
 		}
 	}
