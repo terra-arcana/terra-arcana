e0321777ec2f9b41d15a49ebf678573d04a01fd2
Frédéric Bolduc <bolducfrederic@gmail.com>
2017-02-04 17:45:43 -0500

    Upcoming campaign banner in homepage

--- app/scripts/index.page.jsx
+++ app/scripts/index.page.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 import PageHeader from './layout/page-header.jsx';
 import Spinner from './layout/spinner.jsx';
+
+require('../styles/index.scss');
 
 /**
  * An IndexPage is the main view for displaying the homepage.
@@ -21,7 +24,9 @@ export default class IndexPage extends React.Component {
 		 * @private
 		 */
 		this.state = {
-			posts: []
+			posts: [],
+			campaign: null,
+			campaignNextEvent: null
 		};
 	}
 
@@ -29,11 +34,37 @@ export default class IndexPage extends React.Component {
 	 * @override
 	 */
 	componentDidMount() {
-		jQuery.get(WP_API_Settings.root + 'wp/v2/posts', function(result) {
+		var postsData = [],
+			campaignData = {},
+			campaignNextEventData = {},
+
+			newsRequest = jQuery.get(WP_API_Settings.root + 'wp/v2/posts?per_page=3', result => {
+				postsData = result;
+			}),
+
+			campaignRequest = () => {
+				let deferred = jQuery.Deferred();
+
+				jQuery.get(WP_API_Settings.root + 'wp/v2/campaign?per_page=1', result => {
+					campaignData = result[0];
+
+					// TODO: Handle next campaign event
+					jQuery.get(WP_API_Settings.root + 'terraarcana/v1/campaign-events/' + campaignData.id, result => {
+						campaignNextEventData = result[0];
+						deferred.resolve();
+					});
+				});
+
+				return deferred.promise();
+			};
+
+		jQuery.when(newsRequest, campaignRequest()).done(() => {
 			this.setState({
-				posts: result
+				posts: postsData,
+				campaign: campaignData,
+				campaignNextEvent: campaignNextEventData
 			});
-		}.bind(this));
+		});
 	}
 
 	/**
@@ -41,25 +72,58 @@ export default class IndexPage extends React.Component {
 	 * @return {jsx} The component template
 	 */
 	render() {
-		var content = <Spinner />;
+		var banner = null,
+			content = <Spinner />;
+
+		if (this.state.campaign !== null && this.state.campaignNextEvent !== null) {
+			let date = this.state.campaignNextEvent.date;
+
+			banner = (
+				<div className="col-xs-12">
+					<Link to={this.state.campaign.link}>
+						<div className="ta-campaign-banner-hover-text text-center">
+							<div>
+								<em>prochaine campagne</em>
+								<h2>{this.state.campaign.title.rendered}</h2>
+								<em>du {date.start.rendered} au {date.end.rendered}</em>
+							</div>
+						</div>
+						<img src={this.state.campaign.banner.url} alt={this.state.campaign.title.rendered} />
+					</Link>
+				</div>
+			);
+		}
 
 		if (this.state.posts.length) {
 			content = (
 				<div className="ta-index-content container">
-					{this.state.posts.map(function(post) {
-						return (
-							<div key={post.id} className="ta-post">
-								<div className="page-header">
-									<h2>{post.title.rendered}</h2>
-								</div>
+					<div className="row">
+						<div className="col-xs-12 col-lg-8">
+							<strong>Terra Arcana</strong>
+						</div>
 
-								<div
-									className = "ta-post-content"
-									dangerouslySetInnerHTML = {{__html: post.content.rendered}}
-								/>
-							</div>
-						);
-					}.bind(this))}
+						<div className="col-xs-12 col-lg-4">
+							<h2>Actualités</h2>
+							<ul className="list-unstyled">
+								{this.state.posts.map(function(post) {
+									return (
+										<li
+											key = {post.id}
+											className = "panel panel-default"
+											>
+											<div className="panel-heading">
+												<h3 className="panel-title">{post.title.rendered}</h3>
+											</div>
+											<div
+												className = "panel-body"
+												dangerouslySetInnerHTML = {{__html: post.content.rendered}}
+												/>
+										</li>
+									);
+								}.bind(this))}
+							</ul>
+						</div>
+					</div>
 				</div>
 			);
 		}
@@ -69,6 +133,13 @@ export default class IndexPage extends React.Component {
 				<PageHeader
 					content = "Terra Arcana <small>Grandeur Nature médiéval fantastique</small>"
 				/>
+				<div className="ta-campaign-background">
+					<div className="container">
+						<div className="row">
+							{banner}
+						</div>
+					</div>
+				</div>
 				{content}
 			</div>
 		);
